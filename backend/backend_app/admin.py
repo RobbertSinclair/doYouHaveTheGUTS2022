3c834516c4fdd1255b873221bd067bf28b289043
Robbert Sinclair <2432710S@student.gla.ac.uk>
2022-02-12 13:29:54 +0000

    Pls no key commit

--- backend/backend_app/admin.py
+++ backend/backend_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from backend_app.models import *
 
 # Register your models here.
+
+admin.site.register(Restaurant)
+admin.site.register(UserProfile)
+admin.site.register(Menu)
+admin.site.register(DietTag)
+admin.site.register(Team)
+
+
